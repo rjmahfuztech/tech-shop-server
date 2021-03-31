0fedd16f10a178b15604eb2b03d5fba44a9c1eec
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-03-31 11:15:00 +0600

    all delete

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5500;
-
-app.get('/', (req, res) => {
-  res.send('Hello World! I am Mahfuz')
-})
-
-app.listen(port);
