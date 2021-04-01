61a42f21d1898f8d5c9aa9343ec59eb1bad66abd
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-04-02 00:44:30 +0600

    have some work

--- index.js
+++ index.js
@@ -15,20 +15,42 @@ const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productCollection = client.db("tech").collection("products");
-  
+  const orderCollection = client.db("tech").collection("orders");
+
   app.post('/addProduct', (req, res) => {
-      const newProduct = req.body;
-      console.log('add Product', newProduct);
-      productCollection.insertOne(newProduct)
+    const newProduct = req.body;
+    console.log('add Product', newProduct);
+    productCollection.insertOne(newProduct)
       .then(result => {
-          res.send(result.insertedCount> 0);
+        console.log(result.insertedCount > 0);
+        res.send(result.insertedCount > 0);
       })
   })
 
+
   app.get('/products', (req, res) => {
-    productCollection.find()
-    .toArray( (err, productInfo) => {
+    productCollection.find({})
+      .toArray((err, productInfo) => {
         res.send(productInfo);
+      })
+  })
+
+  app.post('/addOrder', (req, res) => {
+    const newOrder = req.body;
+    console.log('newOrder', newOrder);
+    orderCollection.insertOne(newOrder)
+      .then(result => {
+        console.log(result.insertedCount > 0);
+        res.send(result.insertedCount > 0);
+      })
+  })
+
+
+  app.get('/orders', (req, res) => {
+    orderCollection.find({})
+    .toArray((err, document) => {
+      console.log('ordered', document);
+      // res.send('ordered', document);
     })
   })
 
